1751858c2daadac6cc5cd416e9c8bdc8e2a0459d
Knute Lingaard <Knute.Lingaard@sifive.com>
2022-11-28 12:13:07 -0600

    Reduce IQ credits by number actually sent

--- core/Fetch.cpp
+++ core/Fetch.cpp
@@ -73,7 +73,7 @@ namespace olympia
 
         out_fetch_queue_write_.send(insts_to_send);
 
-        credits_inst_queue_ -= upper;
+        credits_inst_queue_ -= insts_to_send->size();
         if(credits_inst_queue_ > 0) {
             fetch_inst_event_->schedule(1);
         }

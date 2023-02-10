86d828ffd655803053c14d778584d73146f5c0a1
Knute Lingaard <Knute.Lingaard@sifive.com>
2023-02-10 15:26:15 -0600

    Rearranged some builds

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo "Starting Build Entry"
 echo "HOME:" $HOME
@@ -18,8 +18,8 @@ mkdir -p release  # Link step expects "release" as dir name
 ln -s release fastdebug
 ln -s release debug
 cd release
-CC=clang CXX=clang++ cmake .. -DCMAKE_BUILD_TYPE=$OLYMPIA_BUILD_TYPE -DGEN_DEBUG_INFO=OFF -GNinja
-ninja
+CC=clang CXX=clang++ cmake .. -DCMAKE_BUILD_TYPE=$OLYMPIA_BUILD_TYPE -DGEN_DEBUG_INFO=OFF
+make -j2
 BUILD_SPARTA=$?
 if [ ${BUILD_SPARTA} -ne 0 ]; then
     echo "ERROR: build sparta FAILED!!!"

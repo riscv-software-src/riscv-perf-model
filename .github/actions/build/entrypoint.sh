80ebb5cd4fd56f33d78b6ab902dd482dd184a1ae
Knute Lingaard <klingaard@gmail.com>
2023-12-14 11:27:26 -0600

    Sparta and olympia built with same compiler

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -12,6 +12,9 @@ echo "GITHUB_EVENT_PATH:" $GITHUB_EVENT_PATH
 echo "CONDA_PREFIX:" $CONDA_PREFIX
 echo "PWD:" `pwd`
 
+CXX_COMPILER=${COMPILER/clang/clang++}
+CXX_COMPILER=${CXX_COMPILER/gcc/g++}
+
 #
 # Compile Sparta Infra (always build with release)
 #   Have other build types point to release
@@ -20,7 +23,7 @@ echo "Building Sparta Infra"
 cd ${GITHUB_WORKSPACE}/map/sparta
 mkdir -p release
 cd release
-cmake .. -DCMAKE_BUILD_TYPE=$OLYMPIA_BUILD_TYPE -DGEN_DEBUG_INFO=OFF -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}
+CC=$COMPILER CXX=$CXX_COMPILER cmake .. -DCMAKE_BUILD_TYPE=$OLYMPIA_BUILD_TYPE -DGEN_DEBUG_INFO=OFF -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}
 if [ $? -ne 0 ]; then
     echo "ERROR: Cmake for Sparta framework failed"
     exit 1
@@ -35,9 +38,6 @@ fi
 cd ${GITHUB_WORKSPACE}
 mkdir $OLYMPIA_BUILD_TYPE
 cd $OLYMPIA_BUILD_TYPE
-CXX_COMPILER=${COMPILER/clang/clang++}
-CXX_COMPILER=${CXX_COMPILER/gcc/g++}
-
 CC=$COMPILER CXX=$CXX_COMPILER cmake .. -DCMAKE_BUILD_TYPE=$OLYMPIA_BUILD_TYPE -DGEN_DEBUG_INFO=OFF
 if [ $? -ne 0 ]; then
     echo "ERROR: Cmake for olympia failed"

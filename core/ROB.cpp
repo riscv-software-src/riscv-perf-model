daf47a5a47be5466fc1ef30f3b4c0c68c0ccb221
Anthony Hung <anthony.hung@condorcomputing.com>
2024-03-07 23:23:33 -0800

    All instructions including SYS can possibly go through register rename. A problem occurs when a SYS instr gets a renamed dest register. SYS instrutions do not go in any execution pipe.  In current code renamed registers can only be made ready in an excution pipe.  So SYS instructions with dest register rename can never become ready. So any subsequent instruction  waiting on the destination register just hangs.  The solution I made was to give the ROB a scoreboard view and make the dest register ready when the ROB does the flush upon retiring SYS instructions.   In an unrelated fix in the same block of code I also added a line to set the expected_flush_ variable to  true or else the flush fails.

--- core/ROB.cpp
+++ core/ROB.cpp
@@ -72,6 +72,7 @@ namespace olympia
 
     void ROB::sendInitialCredits_()
     {
+        setup_scoreboard_view() ;
         out_reorder_buffer_credits_.send(reorder_buffer_.capacity());
         ev_ensure_forward_progress_.schedule(retire_timeout_interval_);
     }
@@ -188,13 +189,7 @@ namespace olympia
                 // This is rare for the example
                 if(SPARTA_EXPECT_FALSE(ex_inst.getPipe() == InstArchInfo::TargetPipe::SYS))
                 {
-                    ILOG("Instigating flush... " << ex_inst);
-
-                    FlushManager::FlushingCriteria criteria(FlushManager::FlushCause::POST_SYNC, ex_inst_ptr);
-                    out_retire_flush_.send(criteria);
-
-                    ++num_flushes_;
-                    break;
+                  retireSysInst(ex_inst_ptr);
                 }
             }
             else {
@@ -248,4 +243,44 @@ namespace olympia
         }
     }
 
+    // sys instr doesn't have a pipe so we handle special stuff here
+    void ROB::retireSysInst(InstPtr &ex_inst)
+    {
+        auto reg_file = ex_inst->getRenameData().getDestination().rf;
+        if (reg_file == core_types::RegFile::RF_INVALID)
+        {   // this is the case if dst = x0
+            DLOG("retiring SYS instr ");
+        } else   // this is needed or else destination register is 
+        {        // forever reserved and not ready
+            const auto & dest_bits = ex_inst->getDestRegisterBitMask(reg_file);
+            scoreboard_views_[reg_file]->setReady(dest_bits);
+            DLOG("retiring SYS inst dest reg bits: " <<  sparta::printBitSet(dest_bits));
+        }
+
+        FlushManager::FlushingCriteria criteria(FlushManager::FlushCause::POST_SYNC, ex_inst);
+                    out_retire_flush_.send(criteria);
+                    expect_flush_ = true;
+                    ++num_flushes_;
+                    ILOG("Instigating flush due to SYS instruction... " << *ex_inst);
+
+
+    }
+
+    // for SYS instr which doesn't have an exe pipe
+    void ROB::setup_scoreboard_view()
+    {
+        const char QNAME[] =  "iq0";
+        auto cpu_node = getContainer()->findAncestorByName("core.*");
+        if (cpu_node == nullptr)
+        {
+            cpu_node = getContainer()->getRoot();
+        }
+        const auto& rf = core_types::RF_INTEGER;
+
+        // alu0, alu1 name is based on exe names, point to issue_queue name instead
+        DLOG("setup sb view: " << QNAME );
+        scoreboard_views_[rf].reset(
+            new sparta::ScoreboardView(QNAME, core_types::regfile_names[rf],
+                                       cpu_node)); // name needs to come from issue_queue
+    }
 }

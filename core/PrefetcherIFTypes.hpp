b92a5898910c3e518ffa32bffd8980787621a437
Rajat Bhatia <rbhatia@ventanamicro.com>
2024-07-10 16:24:39 -0700

    Clang formatted the prefetcher files

--- core/PrefetcherIFTypes.hpp
+++ core/PrefetcherIFTypes.hpp
@@ -10,19 +10,20 @@
 namespace olympia
 {
 
-/**
- * \class NullPrefetcherStateUpdateType
- *
- * \brief Default prefetcher statue update type, used
- *        as a template type for PrefetcherIF
- *
- * Default state update class used as function parameter for simple prefetchers
- * without outside (of prefetcher) dependencies.
- */
-class NullPrefetcherStateUpdateType {
-public:
-  //! \brief default constructor
-  NullPrefetcherStateUpdateType() = default;
-}; // class NullPrefetcherStateUpdateType
+    /**
+     * \class NullPrefetcherStateUpdateType
+     *
+     * \brief Default prefetcher statue update type, used
+     *        as a template type for PrefetcherIF
+     *
+     * Default state update class used as function parameter for simple prefetchers
+     * without outside (of prefetcher) dependencies.
+     */
+    class NullPrefetcherStateUpdateType
+    {
+      public:
+        //! \brief default constructor
+        NullPrefetcherStateUpdateType() = default;
+    }; // class NullPrefetcherStateUpdateType
 
 } // namespace olympia

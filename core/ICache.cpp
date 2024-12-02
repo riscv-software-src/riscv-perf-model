28f40ad4a9a7cca69d5fc3fbe870b37b9dcbb34b
Govardhan <sai.govardhan@incoresemi.com>
2024-12-02 22:35:49 +0530

    minor fix - consistency of parameter names - replacement

--- core/ICache.cpp
+++ core/ICache.cpp
@@ -37,7 +37,7 @@ namespace olympia {
         const uint32_t l1_line_size = p->l1_line_size;
         const uint32_t l1_size_kb = p->l1_size_kb;
         const uint32_t l1_associativity = p->l1_associativity;
-        const std::string& replacement_policy = p->l1_replacement_policy;
+        const std::string& replacement_policy = p->replacement_policy;
         l1_cache_.reset(new CacheFuncModel(getContainer(), l1_size_kb, l1_line_size, replacement_policy, l1_associativity));
 
         sparta::StartupEvent(node, CREATE_SPARTA_HANDLER(ICache, sendInitialCredits_));

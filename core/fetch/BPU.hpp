e1b343d7f3665b897a1132eeb1f522ad46308a00
Shobhit <shobhit32t@gmail.com>
2025-02-03 05:02:03 +0530

    Added BPU counters

--- core/fetch/BPU.hpp
+++ core/fetch/BPU.hpp
@@ -22,7 +22,11 @@ namespace olympia
 
             uint64_t PC_;
             uint8_t instType_;
-            friend std::ostream & operator<<(std::ostream &, const PredictionRequest &);
+
+            friend std::ostream & operator<<(std::ostream & os, const PredictionRequest &)
+            {
+                return os;
+            }
         };
 
         class PredictionOutput
@@ -32,7 +36,11 @@ namespace olympia
 
             bool predDirection_;
             uint64_t predPC_;
-            friend std::ostream & operator<<(std::ostream &, const PredictionOutput &);
+
+            friend std::ostream & operator<<(std::ostream & os, const PredictionOutput &)
+            {
+                return os;
+            }
         };
 
         class UpdateInput
@@ -43,7 +51,8 @@ namespace olympia
             uint64_t instrPC_;
             bool correctedDirection_;
             uint64_t correctedTargetPC_;
-            friend std::ostream & operator<<(std::ostream &, const UpdateInput &);
+
+            friend std::ostream & operator<<(std::ostream & os, const UpdateInput &) { return os; }
         };
 
         class BPU :
@@ -118,6 +127,54 @@ namespace olympia
                 &unit_event_set_, "ev_sendPrediction",
                 CREATE_SPARTA_HANDLER_WITH_DATA(BPU, sendPrediction_, PredictionOutput)};
                 ***/
+
+            //////////////////////////////////////////////////////////////////////////////
+            // Counters
+            //////////////////////////////////////////////////////////////////////////////
+            sparta::Counter pred_req_num_{getStatisticSet(), "pred_req_num",
+                                          "Number of prediction requests",
+                                          sparta::Counter::COUNT_NORMAL};
+            sparta::Counter mispred_num_{getStatisticSet(), "mispred_num",
+                                         "Number of mis-predictions",
+                                         sparta::Counter::COUNT_NORMAL};
+            sparta::StatisticDef mispred_ratio_{getStatisticSet(), "mispred_ratio",
+                                                "Percenatge of mis-prediction", getStatisticSet(),
+                                                "mispred_num/pred_req_num"};
+            sparta::Counter branch_req_num_{getStatisticSet(), "branch_req_num",
+                                            "Number of branch requests",
+                                            sparta::Counter::COUNT_NORMAL};
+            sparta::Counter call_req_num_{getStatisticSet(), "call_req_num",
+                                          "Number of call requests", sparta::Counter::COUNT_NORMAL};
+            sparta::Counter return_req_num_{getStatisticSet(), "return_req_num",
+                                            "Number of return requests",
+                                            sparta::Counter::COUNT_NORMAL};
+            sparta::Counter pht_req_num_{getStatisticSet(), "pht_req_num",
+                                         "Number of requests made to PHT",
+                                         sparta::Counter::COUNT_NORMAL};
+            sparta::Counter pht_hit_num_{getStatisticSet(), "pht_hit_num", "Number of hits on PHT",
+                                         sparta::Counter::COUNT_NORMAL};
+            sparta::Counter pht_miss_num_{getStatisticSet(), "pht_miss_num",
+                                          "Number of misses on PHT", sparta::Counter::COUNT_NORMAL};
+            sparta::StatisticDef pht_mispred_ratio_{getStatisticSet(), "pht_mispred_ratio",
+                                                    "Percentage of PHT mis-prediction",
+                                                    getStatisticSet(), "pht_miss_num/pht_req_num"};
+            sparta::Counter btb_req_num_{getStatisticSet(), "btb_req_num",
+                                         "Number of requests to BTB",
+                                         sparta::Counter::COUNT_NORMAL};
+            sparta::Counter btb_hit_num_{getStatisticSet(), "btb_hit_num", "NUmber of BTB hits",
+                                         sparta::Counter::COUNT_NORMAL};
+            sparta::Counter btb_miss_num_{getStatisticSet(), "btb_miss_num", "Number of BTB misses",
+                                          sparta::Counter::COUNT_NORMAL};
+            sparta::StatisticDef btb_hit_rate_{getStatisticSet(), "btb_hit_rate",
+                                               "Rate of BTB hits", getStatisticSet(),
+                                               "btb_hit_num/btb_req_num"};
+            sparta::StatisticDef btb_miss_rate_{getStatisticSet(), "btb_miss_rate",
+                                                "Rate of BTB misses", getStatisticSet(),
+                                                "btb_miss_num/btb_req_num"};
+            sparta::Counter ras_high_mark_{getStatisticSet(), "ras_high_mark", "RAS high mark",
+                                           sparta::Counter::COUNT_NORMAL};
+            sparta::Counter ras_low_mark_{getStatisticSet(), "ras_low_mark", "RAS low mark",
+                                          sparta::Counter::COUNT_NORMAL};
         };
     } // namespace BranchPredictor
 } // namespace olympia

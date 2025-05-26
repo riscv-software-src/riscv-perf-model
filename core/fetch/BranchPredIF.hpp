bbd00bac61f71ec1c52dd3e93ab6f672c6b11f21
jeffnye-gh <112903691+jeffnye-gh@users.noreply.github.com>
2025-05-26 16:42:52 -0500

    draft - support multiple predictions
    support for multiple prediction requests
    support for signaling for stage prediction results and updates

--- core/fetch/BranchPredIF.hpp
+++ core/fetch/BranchPredIF.hpp
@@ -22,6 +22,13 @@
  * It is intended that an implementation of branch predictor must also specify
  * implementations of Prediction output, Prediction input and Update input, along with
  * implementations of getPrediction and updatePredictor operations.
+ *
+ * Support for multiple predictions/updates is added parallel to the single 
+ * prediction/update interface
+ * 
+ * A optional signaling interface is proposed for support of prediction override 
+ * in the multi-prediction case
+ * 
  * */
 #pragma once
 
@@ -30,15 +37,43 @@ namespace olympia
 namespace BranchPredictor
 {
 
-    template <class PredictionT, class UpdateT, class InputT>
+    template <class PredictionT, class UpdateT, class InputT, class SignalT>
     class BranchPredictorIF
     {
+      using NInputT = std::vector<InputT>;
+      using NPredictionT = std::vector<PredictionT>;
+      using NUpdateT = std::vector<UpdateT>;
+      using NSignalT = std::map<std::string,SignalT>;
     public:
         // TODO: create constexpr for bytes per compressed and uncompressed inst
         static constexpr uint8_t bytes_per_inst = 4;
-        virtual ~BranchPredictorIF() { };
+        virtual ~BranchPredictorIF() = default;
+
         virtual PredictionT getPrediction(const InputT &) = 0;
         virtual void updatePredictor(const UpdateT &) = 0;
+
+        //N-prediction requests
+        virtual NPredictionT getPrediction(const NInputT &inputs) {
+          std::vector<PredictionT> result;
+          result.reserve(inputs.size());
+          for (const auto &input : inputs)
+              result.push_back(getPrediction(input));
+          return result;
+        }
+
+        //N-update requests
+        virtual void updatePredictor(const NUpdateT &updates) {
+            for (const auto &update : updates)
+                updatePredictor(update);
+        }
+
+        //Optional signal interface for staging predictions
+        virtual std::optional<NSignalT> getSignals() const {
+           return std::nullopt;
+        }
+
+        // Name the predictor
+        virtual std::string getName() const = 0;
     };
 
 } // namespace BranchPredictor

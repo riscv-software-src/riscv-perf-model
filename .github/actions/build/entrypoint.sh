eb64cc5f88746e86129e48912e3278490e194f21
Knute Lingaard <klingaard@gmail.com>
2025-10-25 12:44:20 -0500

    Removed need for conda environment

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -2,14 +2,10 @@
 
 set -x
 
-source $CONDA/etc/profile.d/conda.sh
-conda activate riscv_perf_model
-
 echo "Starting Build Entry"
 echo "HOME:" $HOME
 echo "GITHUB_WORKSPACE:" $GITHUB_WORKSPACE
 echo "GITHUB_EVENT_PATH:" $GITHUB_EVENT_PATH
-echo "CONDA_PREFIX:" $CONDA_PREFIX
 echo "PWD:" `pwd`
 
 CXX_COMPILER=${COMPILER/clang/clang++}
@@ -28,7 +24,7 @@ echo "Building Sparta Infra"
 cd ${GITHUB_WORKSPACE}/map/sparta
 mkdir -p release
 cd release
-CC=$COMPILER CXX=$CXX_COMPILER cmake .. -DCMAKE_BUILD_TYPE=Release -DGEN_DEBUG_INFO=OFF -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}
+CC=$COMPILER CXX=$CXX_COMPILER cmake .. -DCMAKE_BUILD_TYPE=Release -DGEN_DEBUG_INFO=OFF
 if [ $? -ne 0 ]; then
     echo "ERROR: Cmake for Sparta framework failed"
     exit 1
